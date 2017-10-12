a96d46282058f555b69dfbf9e44a7d48fedbffc7
Juan Estrella <juan.estrella@axomic.com>
2017-10-12 11:33:27 -0400

    Bug fix: Projects constructor couldn't handle project codes passed as floats instead of strings => 1234.00 instead of '1234.00' would cause an error.

--- lib/Nouns/Projects.rb
+++ lib/Nouns/Projects.rb
@@ -16,17 +16,19 @@ class Projects
     #         proj =  Projects.new('My Project','1234.00')
     def initialize(*args)
         
-        if args.length > 1 #We only want one arguement or 2 non-null ones
-            unless args.length == 2 && !args.include?(nil) && (args[0].is_a?(String) || args[0].is_a?(Integer)) && (args[1].is_a?(String) || args[1].is_a?(Integer))
+        if args.length > 1 #We only want 2 non-null ones
+            unless args.length == 2 && !args.include?(nil) && 
+                  (args[0].is_a?(String) || args[0].is_a?(Integer) || args[0].is_a?(Float)) && 
+                  (args[1].is_a?(String) || args[1].is_a?(Integer) || args[1].is_a?(Float))
                 warn "Argument Error:\n\tExpected either\n\t1. No Arguments\n\t2. A Hash\n\t" + 
                      "3. Two separate string arguments." +
                      " e.g. Projects.new(name,code) in that order." + 
                      "\n\tInstead got #{args.inspect} => Creating empty ProjectKeywords object."
             else
-                #grab the agruments and set up the json object
+                #grab the first two agruments and set up the json object
                 json_obj = {"name" => args[0].to_s, "code" => args[1].to_s}
             end
-        else
+        else # If a Hash or No argument is passed to the constructor
             json_obj = Validator::validate_argument(args.first,'Projects')
         end
         @alive = json_obj['alive']
