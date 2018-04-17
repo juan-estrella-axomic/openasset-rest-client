6b17d1d2b0ff2520eca3a4d79c75c854f6a9f1b4
Juan Estrella <juan.estrella@axomic.com>
2018-04-17 09:59:12 -0400

    Removed unecessary filter when getting instance variables from object in export to csv - Removed adding date to folder name in array downloader method - Updated downloader class to handle socket error and general exceptions by retryin multiple time before aborting. - Updated version to 1.1.3

--- lib/Downloader.rb
+++ lib/Downloader.rb
@@ -3,6 +3,7 @@ require 'ruby-progressbar'
 
 require_relative 'Validator'
 require_relative 'MyLogger'
+require_relative 'SmartUpdater'
 
 class Downloader
 
@@ -11,38 +12,57 @@ class Downloader
         resource = uri.to_s.split('/').last
         filename = location.split('/').last
 
-        Logging::logger.info( "Downloading file => #{filename}")
+        Logging.logger.info( "Downloading file => #{filename}")
 
-        Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-            # Perform head request to get the content length to feed the progress bar      
-            res = http.request_head(uri)
-            file_size = res['content-length'].to_i
-            response = Validator.process_http_response(res,true,resource,'GET')
-          
-            return response unless response.kind_of? Net::HTTPSuccess
+        begin
 
-            pbar = ProgressBar.create(:format => '%p%% [%b>%i] %r KB/sec  ETA %e',
-                                      :rate_scale => lambda { |rate| rate / 1024 },
-                                      :starting_at => 0, 
-                                      :total => file_size)
+            retries ||= 0
 
-            File.open(location, "wb") do |file|
-                
-                http.request_get(uri.request_uri) do |resp|
+            Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+                # Perform head request to get the content length to feed the progress bar      
+                res = http.request_head(uri)
+                file_size = res['content-length'].to_i
+                response = Validator.process_http_response(res,true,resource,'GET')
+            
+                return response unless response.kind_of? Net::HTTPSuccess
+
+                pbar = ProgressBar.create(:format => '%p%% [%b>%i] %r KB/sec  ETA %e',
+                                        :rate_scale => lambda { |rate| rate / 1024 },
+                                        :starting_at => 0, 
+                                        :total => file_size)
+
+                File.open(location, "wb") do |file|
                     
-                    resp.read_body do |chunk|
-                      
-                        file.write(chunk)
-                        pbar.progress += chunk.size
-                   
+                    http.request_get(uri.request_uri) do |resp|
+                        
+                        resp.read_body do |chunk|
+                        
+                            file.write(chunk)
+                            pbar.progress += chunk.size
+                    
+                        end
+                    
                     end
                 
                 end
-            
-            end
        
+            end
+
+        rescue SocketError => e
+
+            Logging.logger.warn("Socket Error: #{e}.")
+            wait_and_try_again
+            retry if (retries += 1) < 20
+            abort(e)
+
+        rescue Exception => e
+
+            Logging.logger.warn("Exception: #{e}.")
+            wait_and_try_again
+            retry if (retries += 1) < 5
+            abort(e)
+
         end
-    
     end
 
 end
--- lib/Version/version.rb
+++ lib/Version/version.rb
@@ -1,3 +1,3 @@
 module Openasset
-  VERSION = "1.1.2"
+  VERSION = "1.1.3"
 end
