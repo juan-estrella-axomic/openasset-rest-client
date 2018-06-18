a4bb43ea1505a84f4703e876221ff144590b32b1
Juan Estrella <juan.estrella@axomic.com>
2018-06-18 12:58:24 -0400

    set add_files_to_albums method to accept strings, integers,arrays, and objects - Cleaned up error checking logic

--- lib/AdministrativeFunctions/AddFilesToAlbum.rb
+++ lib/AdministrativeFunctions/AddFilesToAlbum.rb
@@ -1,11 +1,20 @@
 module AddFilesToAlbum
 
-	def __add_files_to_album(albums=nil,files=nil)
-        
-            return if albums.to_s.eql?('') && files.to_s.eql?('')
+	def __add_files_to_album(albums='',files='')
+
+            if albums.empty?
+                logger.error("Albums argument cannot be empty.")
+                return false
+            end
+
+            if files.empty?
+                logger.error("Files argument cannot be empty.")
+                return false
+            end
             # Get album objects
-            if albums.is_a?(String) || albums.is_a?(Integer)
-                
+            if albums.is_a?(Albums)
+                albums = [albums]
+            elsif albums.is_a?(String) || albums.is_a?(Integer)
                 ids = albums.to_s.split(/,/).reject { |v| v.strip.empty? || v.to_i.eql?(0) }
                 op = RestOptions.new
                 op.add_option('id',ids)
@@ -15,25 +24,26 @@ module AddFilesToAlbum
                 if albums.first.is_a?(String) || albums.first.is_a?(Integer)
                     op = RestOptions.new
                     op.add_option('id',albums)
-                    albums = get_albums(op) 
+                    albums = get_albums(op)
                     op.clear
-                    return false if albums.empty?
+                    if albums.empty?
+                        logger.error("Albums with id(s) => #{albums.join(',')} not found.")
+                        return false
+                    end
                 end
-                unless albums.first.is_a?(Albums) || albums.first.is_a?(String) || albums.first.is_a?(Integer)
-                    logger.error("Expected Albums object(s), string id(s), array of id(s) for first argument. Instead Got => #{albums.to_s.inspect}.")
-                    return false
-                end
-            end
-
-            if albums.empty?
-                logger.error("Album insertion error: Album(s) not found in OpenAsset.")
+            else
+                logger.error("Expected Albums object(s), string id(s), array of id(s) for first argument. " +
+                             "Instead Got => #{albums.to_s.inspect}.")
                 return false
             end
+
             logger.info("Retrieved album(s).")
 
 
             # Get File objects
-            if files.is_a?(String) || files.is_a?(Integer)
+            if files.is_a?(Files)
+                files = [files]
+            elsif files.is_a?(String) || files.is_a?(Integer)
                 ids = files.to_s.split(/,/).reject { |v| v.strip.empty? || v.to_i.eql?(0) }
                 op = RestOptions.new
                 op.add_option('id',ids)
@@ -43,27 +53,26 @@ module AddFilesToAlbum
                 if files.first.is_a?(String) || files.first.is_a?(Integer)
                     op = RestOptions.new
                     op.add_option('id',files)
-                    files = get_albums(op) 
+                    files = get_albums(op)
                     op.clear
-                    return false if files.empty?
+                    if files.empty?
+                        logger.error("Files with id(s) => #{files.join(',')} not found.")
+                        return false
+                    end
                 end
-                unless files.first.is_a?(Files) || files.first.is_a?(String) || files.first.is_a?(Integer)
-                    logger.error("Expected Files object(s), string id(s), array of id(s) for second argument. Instead Got => #{files.to_s.inspect}.")
-                    return false
-                end
-            end
-    
-            if files.empty?
-                logger.error("Album insertion error: File(s) not found in OpenAsset.")
+            else
+                logger.error("Expected Files object(s), string id(s), array of id(s) for second argument. " +
+                             "Instead Got => #{files.to_s.inspect}.")
                 return false
             end
+
             logger.info("Retrieved album(s).")
 
             # Loop through albums and add files
             logger.info("Adding files to album.")
             albums.each do |album|
                 uri = URI.parse(@uri + "/Albums" + "/#{album.id}" + "/Files")
-                res = post(uri,files,false)      
+                res = post(uri,files,false)
                 return false unless res.kind_of?(Net::HTTPSuccess)
             end
             return true
