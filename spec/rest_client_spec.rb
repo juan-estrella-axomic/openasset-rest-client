b5eae366c17e957bf2b7224dae9dbcbe2d795c83
Juan Estrella <juan.estrella@columbia.edu>
2018-06-30 00:14:29 -0400

    Started adding Rspec Unit Testing suite

---
+++ spec/rest_client_spec.rb
@@ -0,0 +1,49 @@
+require_relative '../openasset-rest-client'
+
+include OpenAsset
+
+describe RestClient do
+
+    let(:client) { RestClient.new('demo-jes.openasset.com','rspec_user','') } 
+
+    context 'when dealing with access levels' do
+
+    end
+
+    context 'when dealing with albums' do
+
+    end
+
+    context 'when dealing with alternate stores' do
+
+    end
+
+    context 'when dealing with aspect ratios' do
+
+    end
+
+    context 'when dealing with categories' do
+
+    end
+
+    context 'when dealing with copyright holders' do
+
+    end
+
+    context 'when dealing with copyright policies' do
+
+    end
+
+    context 'when dealing with data integrations' do
+
+    end
+
+    context 'when dealing with fields' do
+
+    end
+
+    context 'when dealing with field lookup strings' do
+
+    end
+
+end
