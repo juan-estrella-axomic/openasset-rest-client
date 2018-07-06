f9cd3210e15f09db405cf4d28fcaa58d5a5294d8
Juan Estrella <juan.estrella@axomic.com>
2018-07-06 12:16:53 -0400

    -

--- lib/Nouns/AccessLevels.rb
+++ lib/Nouns/AccessLevels.rb
@@ -1,7 +1,7 @@
 # AccessLevels class
-# 
+#
 # @author Juan Estrella
-
+require_relative '../Validator'
 class AccessLevels
 
     # @!parse attr_accessor :id, :label
@@ -9,16 +9,16 @@ class AccessLevels
 
     # Creates an AccessLevels object
     #
-    # @param data [Hash, nil] Takes a JSON object/Hash or no argument 
+    # @param data [Hash, nil] Takes a JSON object/Hash or no argument
     # @return [AccessLevels object]
     #
-    # @example 
+    # @example
     #         access_level = AccessLevels.new
     def initialize(data=nil)
         json_obj = Validator.validate_argument(data,'AccessLevels')
-        @id      = json_obj['id']                    
+        @id      = json_obj['id']
         @label   = json_obj['label']
-    end                
+    end
 
     # @!visibility private
     def json
--- spec/access_levels_spec.rb
+++ spec/access_levels_spec.rb
@@ -2,25 +2,22 @@ require_relative 'spec_helper'
 require_relative '../lib/Nouns/AccessLevels'
 
 RSpec.describe AccessLevels do
-
+    let(:access_level) { AccessLevels.new }
     describe '#id' do
-        let(:access_level) { AccessLevels.new }
-        access_level.id = '10'
         it 'sets and returns the id' do
+            access_level.id = '10'
             expect(access_level.id).to eq '10'
         end
     end
     describe '#label' do
-        let(:access_level) { AccessLevels.new }
-        access_level.label = 'RSpecTest'
         it 'sets and returns the label' do
+            access_level.label = 'RSpecTest'
             expect(access_level.label).to eq 'RSpecTest'
         end
     end
     describe '#json' do
-        let(:access_level) { AccessLevels.new }
         it 'converts the object to json' do
-            expect(access_level.json.is_a(Hash)).to be true
+            expect(access_level.json.is_a?(Hash)).to be true
         end
     end
 end
