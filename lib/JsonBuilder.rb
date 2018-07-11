84b10907a29765a49a9be7db380b1ae2704ca531
Juan Estrella <juan.estrella@axomic.com>
2018-07-11 10:58:27 -0400

    Updated JsonBuilder to maintain data types returned by api. Removed unecessary logic check for arrays. Integers no longer represented in string form.

--- lib/JsonBuilder.rb
+++ lib/JsonBuilder.rb
@@ -6,15 +6,15 @@ module JsonBuilder
             value = self.instance_variable_get(var)
             next if value.nil?
             key = var.gsub('@','').to_sym
-            if value.is_a?(String) || value.is_a?(Integer)
-                json_obj[key] = value.to_s
-            elsif value.is_a?(Array) && !value.empty?
+            if value.is_a?(String) || value.is_a?(Integer) || values.is_a?(Float)
+                json_obj[key] = value
+            elsif value.is_a?(Array)
                 if value.first.respond_to?(:json)
                     json_obj[key] = value.map { |obj| obj.json }
                 else
                     json_obj[key] = value
                 end
-            elsif value.class.to_s == 'Location'
+            else # Value is an object so recurse
                 json_obj[key] = value.json
             end
         end
