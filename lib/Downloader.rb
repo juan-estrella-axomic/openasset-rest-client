b6eb6da707843b433eb05181995ad7db9a79d8a7
Juan Estrella <juan.estrella@axomic.com>
2018-08-21 11:22:28 -0400

    Changed rescue to use StandardError instead of Exception Use CGI.unescape over obsolete URI.decode. Formatting Changes.

--- lib/Downloader.rb
+++ lib/Downloader.rb
@@ -1,4 +1,4 @@
-require 'uri'
+require 'cgi'
 require 'net/http'
 require 'ruby-progressbar'
 
@@ -17,13 +17,12 @@ class Downloader
         folder_path               = folders.join('/')                      # => Folder where file will be saved -> path/to
 
         possibly_encoded_filename = file_path_components.last              # => tweedle%20dee%20%26%20tweedle%20dum%20%281%29.jpg
-        decoded_filename          = URI.decode(possibly_encoded_filename)  # => tweedle dee & tweedle dum (1).jpg
+        decoded_filename          = GCI.unescape(possibly_encoded_filename)# => tweedle dee & tweedle dum (1).jpg
         decoded_file_path         = folder_path + '/' + decoded_filename   # /path/to/tweedle dee & tweedle dum (1).jpg
 
-        Logging.logger.info( "Downloading file => #{decoded_filename}")
+        Logging.logger.info("Downloading file => #{decoded_filename}")
 
         begin
-
             retries ||= 0
 
             Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
@@ -31,29 +30,22 @@ class Downloader
                 res = http.request_head(uri)
                 file_size = res['content-length'].to_i
                 response = Validator.process_http_response(res,true,resource,'GET')
-            
+
                 return response unless response.kind_of? Net::HTTPSuccess
 
                 pbar = ProgressBar.create(:format => '%p%% [%b>%i] %r KB/sec  ETA %e',
-                                        :rate_scale => lambda { |rate| rate / 1024 },
-                                        :starting_at => 0, 
-                                        :total => file_size)
+                                          :rate_scale => lambda { |rate| rate / 1024 },
+                                          :starting_at => 0, 
+                                          :total => file_size)
 
-                File.open(decoded_file_path, "wb") do |file|
-                    
+                File.open(decoded_file_path, 'wb') do |file|
                     http.request_get(uri.request_uri) do |resp|
-                        
                         resp.read_body do |chunk|
-                        
                             file.write(chunk)
                             pbar.progress += chunk.size
-                    
                         end
-                    
                     end
-                
                 end
-       
             end
 
         rescue SocketError => e
@@ -64,10 +56,10 @@ class Downloader
             retry if (retries += 1) < 20
             abort(e)
 
-        rescue Exception => e
+        rescue StandardError => e
 
             Logging.logger.warn("Exception: #{e}.")
-            Logging.logger.warn( "Path => #{uri}")
+            Logging.logger.warn("Path => #{uri}")
             SmartUpdater.wait_and_try_again
             retry if (retries += 1) < 5
             abort(e)
