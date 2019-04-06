c4827410f08c30b482ce40433c22a1ab39173ef7
Juan Estrella <juan.estrella@columbia.edu>
2019-04-06 14:54:27 -0400

    Fixed issue returning duplicate results

--- lib/Fetcher.rb
+++ lib/Fetcher.rb
@@ -11,17 +11,27 @@ module Fetcher
             o.add_options('limit',0)
             o.add_options('displayFields','id')
         end
-        ids = get(uri,options,false).map { |obj| obj.id }
-        c,r = ids.length.divmod(batch_size)
-        c += 1 if r > 0
+
+        ids = get(uri,options,false).map { |obj| obj.id } # get ids for all object in table
+        iterations,remainder = ids.length.divmod(batch_size)
+        iterations          += 1 if remainder > 0
+
         ids.each_slice(batch_size).with_index(1) do |subset,i|
-            logger.info("Retrieving batch #{i} of #{c}")
+            logger.info("Retrieving batch #{i+1} of #{iterations}")
             options.clear
-            options.add_options('limit',0)
-            options.add_options('id',subset)
-            batch = get(uri,options,true)
+            start_value = subset.first.to_s
+            end_value   = subset.last.to_s
+
+            #id_range = start_value + '-' + end_value => less data in query but runs slower
+            id_range = subset.join(',') # More data sent in query but runs faster => faster wins
+            options.add_raw_options("id=#{id_range}")
+            options.add_raw_options('limit=0')
+
+            batch = get(uri,options,true) # true flag returns objects with ALL fields and sizes
+
             objects.concat(batch)
         end
-        objects
+        # API appears to be caching results. Need to look into this
+        objects = objects.uniq { |o| o.id }
     end
 end
