8759a0165135b1243061a69e47f6ab050566fa2e
Juan Estrella <juan.estrella@axomic.com>
2019-04-25 16:59:44 -0400

    Removed system exit catching in tests bc it breaks the RspecJUnitFormatter XML generator

--- spec/sql_parser_spec.rb
+++ spec/sql_parser_spec.rb
@@ -105,16 +105,12 @@ describe SQLParser do
         it 'catches mismatched parenthesis' do
             p = parser
             query = "where (original_filename in ('one','two','three','in') or original_filename like '%bummy joe%' and id > 47 "
-            expected_result = nil
-            expressions = p.parse(query,'Files')
-            expect(expressions).to eq expected_result
+            expect(p.parse(query,'Files')).to eq nil
         end
         it 'catches missing operators' do
             p = parser
             query = "where (original_filename 'flower') and id = 2 "
-            expected_result = nil
-            expressions = p.parse(query,'Files')
-            expect(expressions).to eq expected_result
+            expect(p.parse(query,'Files')).to eq nil
         end
         it 'handles multiple and statements' do
             p = parser
@@ -128,7 +124,6 @@ describe SQLParser do
             query = "where original_filename = 'flower' or id > 2 or id = 28"
             expected_result = [["", "original_filename", "==", "flower", ""], "or", ["", "id", ">", 2, ""], "or", ["", "id", "==", 28, ""]]
             expressions = p.parse(query,'Files')
-            p expressions
             expect(expressions).to eq expected_result
         end
     end
